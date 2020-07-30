0de5ecca2731434f580c006d7171a65b3505b21e
minara35 <minara35@gmail.com>
2020-07-29 21:54:47 -0400

    first commit

---
+++ src/HelloB20.java
@@ -0,0 +1,10 @@
+public class HelloB20 {
+    public static void main(String[] args) {
+        System.out.println("Hello B20, it is pleasure to be with you!");
+
+
+    // added a line
+        
+
+    }
+}
